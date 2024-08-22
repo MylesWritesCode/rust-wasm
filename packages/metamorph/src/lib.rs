1f9ebacc2e8133637265f1b0c47ef8e79ed46fc3
Myles Berueda <myles@themapletree.io>
2024-08-22 02:06:10 -1000

    adds basic wasm-bindgen example

--- packages/metamorph/src/lib.rs
+++ packages/metamorph/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {name}!"));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
