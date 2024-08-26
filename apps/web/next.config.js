d8d78464141aa61ad38e3aab62d389be1e16953a
Myles Berueda <myles@themapletree.io>
2024-08-26 00:38:22 -1000

    testing async wasm impl with nextjs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,7 +6,23 @@ module.exports = {
 	) => {
 		config.experiments = {
 			...config.experiments,
+			// This is required to be able to use wasm files in the project.
+			// Unfortunately, Nextjs doesn't have the greatest support for WASM. With
+			// Vite, all we had to do is import some plugins, and it worked out of
+			// the box. For now, we'll have to do it like this.
 			syncWebAssembly: true,
+			// Currently causes issues with Next.js. It says the target environment
+			// is not async, so enabling this will cause runtime errors.
+			//
+			// The generated code contains 'async/await' because this module is
+			// using "asyncWebAssembly". However, your target environment does not
+			// appear to support 'async/await'. As a result, the code may not run as
+			// expected or may cause runtime errors. asyncWebAssembly: true,
+			//
+			// Not sure how to get around it, but that's not the main focus of this
+			// project, so I'll leave it for now. I spent way too much time on logs
+			// anyway.
+			// asyncWebAssembly: true,
 		};
 
 		return config;
--- apps/web/src/app/alert/page.tsx
+++ apps/web/src/app/alert/page.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import GreetButton from "./greet-button";
 
 export default function Page() {
 	return (
 		<div className='container'>
-			<div className='flex-1 w-full'>
+			<div className='flex-1 w-full mt-8'>
 				<h1 className='title'>
 					<span>alert</span>
 				</h1>
