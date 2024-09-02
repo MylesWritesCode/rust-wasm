841665fc75f2759b12fcfc625027eb7e5054f130
Myles Berueda <myles@polypixel.io>
2024-09-02 04:24:46 -0700

    feat: adds links to main page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,18 @@
 export const metadata = {
-  title: "rust-wasm",
+  title: 'rust-wasm',
 };
 
 export default function Page(): JSX.Element {
   return (
-    <div className="container">
-      <h1 className="title">
+    <div className='container'>
+      <h1 className='title'>
         <span>rust-wasm</span>
       </h1>
+      <a href='/alert'>alert</a>
+      <a href='/metamorph'>metamorph</a>
+      <a href='https://github.com/MylesWritesCode/rust-wasm' target='_blank' rel='noreferrer'>
+        source
+      </a>
     </div>
   );
 }
